5705368b10629e9383db94c5ae28078b616cf1dc
abdel622 <abdelghaniaitsaid7@gmail.com>
2022-07-14 17:13:45 +0100

    third commit

--- api/services.py
+++ api/services.py
@@ -1,33 +1,33 @@
-from django.conf import settings
-from .utilities import CROPS_DATA_F, Min_THRESH
-from fastai.vision import open_image
-from fastai.basic_train import load_learner
-import os
+# from django.conf import settings
+# from .utilities import CROPS_DATA_F, Min_THRESH
+# from fastai.vision import open_image
+# from fastai.basic_train import load_learner
+# import os
 
 
-def irrigation(sector, moisture):
-    enracinement = CROPS_DATA_F[sector.crop]["enracinement"] 
-    type_soil = sector.farm.soil_type
+# def irrigation(sector, moisture):
+#     enracinement = CROPS_DATA_F[sector.crop]["enracinement"] 
+#     type_soil = sector.farm.soil_type
             
-    thresh = Min_THRESH[enracinement][type_soil]["min"]
-    print(thresh)
+#     thresh = Min_THRESH[enracinement][type_soil]["min"]
+#     print(thresh)
 
-    if (moisture >= 0.95):
-        action = "stop"
-    elif (moisture <= thresh):
-        action = "start"
-    else:
-        action = "nothing"
-    return action
-
-
-def disease_image_detection():
-    img = open_image(os.path.join(os.getcwd(), 'alternaria-tomato.jpg'))
-    learn = load_learner(os.getcwd(), file='leaf_diseases_model.pkl')
-    print(str(learn.predict(img)[0]))
+#     if (moisture >= 0.95):
+#         action = "stop"
+#     elif (moisture <= thresh):
+#         action = "start"
+#     else:
+#         action = "nothing"
+#     return action
 
 
+# def disease_image_detection():
+#     img = open_image(os.path.join(os.getcwd(), 'alternaria-tomato.jpg'))
+#     learn = load_learner(os.getcwd(), file='leaf_diseases_model.pkl')
+#     print(str(learn.predict(img)[0]))
 
-def temperature_ext():
-    pass
+
+
+# def temperature_ext():
+#     pass
 
